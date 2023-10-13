d0a0f9e08370cd6446a230bf40263d7d7508d682
Ben Prystawski <11021815+benpry@users.noreply.github.com>
2023-10-12 17:44:57 -0700

    updated stimuli for pilot

--- all_stimuli.js
+++ all_stimuli.js
@@ -1,1 +1,143 @@
-const stimuli = [{"world": ["If Thing 1 then Thing 2.", "If Thing 2 then Thing 3.", "If Thing 3 then Thing 4.", "If Thing 4 then Thing 5.", "If Thing 5 then Thing 6."], "premise": "Thing 2", "conclusion": "Therefore Thing 1."}, {"world": ["If Thing 1 then Thing 2.", "If Thing 2 then Thing 3.", "If Thing 3 then Thing 4.", "If Thing 4 then Thing 5.", "If Thing 5 then Thing 6."], "premise": "Thing 2", "conclusion": "Therefore not Thing 1."}, {"world": ["If Thing 1 then Thing 2.", "If Thing 2 then Thing 3.", "If Thing 3 then Thing 4.", "If Thing 4 then Thing 5.", "If Thing 5 then Thing 6."], "premise": "not Thing 2", "conclusion": "Therefore Thing 1."}, {"world": ["If Thing 1 then Thing 2.", "If Thing 2 then Thing 3.", "If Thing 3 then Thing 4.", "If Thing 4 then Thing 5.", "If Thing 5 then Thing 6."], "premise": "not Thing 2", "conclusion": "Therefore not Thing 1."}, {"world": ["If Thing 1 then Thing 2.", "If Thing 2 then Thing 3.", "If Thing 3 then Thing 4.", "If Thing 4 then Thing 5.", "If Thing 5 then Thing 6."], "premise": "Thing 3", "conclusion": "Therefore Thing 1."}, {"world": ["If Thing 1 then Thing 2.", "If Thing 2 then Thing 3.", "If Thing 3 then Thing 4.", "If Thing 4 then Thing 5.", "If Thing 5 then Thing 6."], "premise": "Thing 3", "conclusion": "Therefore not Thing 1."}, {"world": ["If Thing 1 then Thing 2.", "If Thing 2 then Thing 3.", "If Thing 3 then Thing 4.", "If Thing 4 then Thing 5.", "If Thing 5 then Thing 6."], "premise": "not Thing 3", "conclusion": "Therefore Thing 1."}, {"world": ["If Thing 1 then Thing 2.", "If Thing 2 then Thing 3.", "If Thing 3 then Thing 4.", "If Thing 4 then Thing 5.", "If Thing 5 then Thing 6."], "premise": "not Thing 3", "conclusion": "Therefore not Thing 1."}, {"world": ["If Thing 1 then Thing 2.", "If Thing 2 then Thing 3.", "If Thing 3 then Thing 4.", "If Thing 4 then Thing 5.", "If Thing 5 then Thing 6."], "premise": "Thing 3", "conclusion": "Therefore Thing 2."}, {"world": ["If Thing 1 then Thing 2.", "If Thing 2 then Thing 3.", "If Thing 3 then Thing 4.", "If Thing 4 then Thing 5.", "If Thing 5 then Thing 6."], "premise": "Thing 3", "conclusion": "Therefore not Thing 2."}, {"world": ["If Thing 1 then Thing 2.", "If Thing 2 then Thing 3.", "If Thing 3 then Thing 4.", "If Thing 4 then Thing 5.", "If Thing 5 then Thing 6."], "premise": "not Thing 3", "conclusion": "Therefore Thing 2."}, {"world": ["If Thing 1 then Thing 2.", "If Thing 2 then Thing 3.", "If Thing 3 then Thing 4.", "If Thing 4 then Thing 5.", "If Thing 5 then Thing 6."], "premise": "not Thing 3", "conclusion": "Therefore not Thing 2."}, {"world": ["If Thing 1 then Thing 2.", "If Thing 2 then Thing 3.", "If Thing 3 then Thing 4.", "If Thing 4 then Thing 5.", "If Thing 5 then Thing 6."], "premise": "Thing 4", "conclusion": "Therefore Thing 1."}, {"world": ["If Thing 1 then Thing 2.", "If Thing 2 then Thing 3.", "If Thing 3 then Thing 4.", "If Thing 4 then Thing 5.", "If Thing 5 then Thing 6."], "premise": "Thing 4", "conclusion": "Therefore not Thing 1."}, {"world": ["If Thing 1 then Thing 2.", "If Thing 2 then Thing 3.", "If Thing 3 then Thing 4.", "If Thing 4 then Thing 5.", "If Thing 5 then Thing 6."], "premise": "not Thing 4", "conclusion": "Therefore Thing 1."}, {"world": ["If Thing 1 then Thing 2.", "If Thing 2 then Thing 3.", "If Thing 3 then Thing 4.", "If Thing 4 then Thing 5.", "If Thing 5 then Thing 6."], "premise": "not Thing 4", "conclusion": "Therefore not Thing 1."}, {"world": ["If Thing 1 then Thing 2.", "If Thing 2 then Thing 3.", "If Thing 3 then Thing 4.", "If Thing 4 then Thing 5.", "If Thing 5 then Thing 6."], "premise": "Thing 4", "conclusion": "Therefore Thing 2."}, {"world": ["If Thing 1 then Thing 2.", "If Thing 2 then Thing 3.", "If Thing 3 then Thing 4.", "If Thing 4 then Thing 5.", "If Thing 5 then Thing 6."], "premise": "Thing 4", "conclusion": "Therefore not Thing 2."}, {"world": ["If Thing 1 then Thing 2.", "If Thing 2 then Thing 3.", "If Thing 3 then Thing 4.", "If Thing 4 then Thing 5.", "If Thing 5 then Thing 6."], "premise": "not Thing 4", "conclusion": "Therefore Thing 2."}, {"world": ["If Thing 1 then Thing 2.", "If Thing 2 then Thing 3.", "If Thing 3 then Thing 4.", "If Thing 4 then Thing 5.", "If Thing 5 then Thing 6."], "premise": "not Thing 4", "conclusion": "Therefore not Thing 2."}, {"world": ["If Thing 1 then Thing 2.", "If Thing 2 then Thing 3.", "If Thing 3 then Thing 4.", "If Thing 4 then Thing 5.", "If Thing 5 then Thing 6."], "premise": "Thing 4", "conclusion": "Therefore Thing 3."}, {"world": ["If Thing 1 then Thing 2.", "If Thing 2 then Thing 3.", "If Thing 3 then Thing 4.", "If Thing 4 then Thing 5.", "If Thing 5 then Thing 6."], "premise": "Thing 4", "conclusion": "Therefore not Thing 3."}, {"world": ["If Thing 1 then Thing 2.", "If Thing 2 then Thing 3.", "If Thing 3 then Thing 4.", "If Thing 4 then Thing 5.", "If Thing 5 then Thing 6."], "premise": "not Thing 4", "conclusion": "Therefore Thing 3."}, {"world": ["If Thing 1 then Thing 2.", "If Thing 2 then Thing 3.", "If Thing 3 then Thing 4.", "If Thing 4 then Thing 5.", "If Thing 5 then Thing 6."], "premise": "not Thing 4", "conclusion": "Therefore not Thing 3."}, {"world": ["If Thing 1 then Thing 2.", "If Thing 2 then Thing 3.", "If Thing 3 then Thing 4.", "If Thing 4 then Thing 5.", "If Thing 5 then Thing 6."], "premise": "Thing 5", "conclusion": "Therefore Thing 1."}, {"world": ["If Thing 1 then Thing 2.", "If Thing 2 then Thing 3.", "If Thing 3 then Thing 4.", "If Thing 4 then Thing 5.", "If Thing 5 then Thing 6."], "premise": "Thing 5", "conclusion": "Therefore not Thing 1."}, {"world": ["If Thing 1 then Thing 2.", "If Thing 2 then Thing 3.", "If Thing 3 then Thing 4.", "If Thing 4 then Thing 5.", "If Thing 5 then Thing 6."], "premise": "not Thing 5", "conclusion": "Therefore Thing 1."}, {"world": ["If Thing 1 then Thing 2.", "If Thing 2 then Thing 3.", "If Thing 3 then Thing 4.", "If Thing 4 then Thing 5.", "If Thing 5 then Thing 6."], "premise": "not Thing 5", "conclusion": "Therefore not Thing 1."}, {"world": ["If Thing 1 then Thing 2.", "If Thing 2 then Thing 3.", "If Thing 3 then Thing 4.", "If Thing 4 then Thing 5.", "If Thing 5 then Thing 6."], "premise": "Thing 5", "conclusion": "Therefore Thing 2."}, {"world": ["If Thing 1 then Thing 2.", "If Thing 2 then Thing 3.", "If Thing 3 then Thing 4.", "If Thing 4 then Thing 5.", "If Thing 5 then Thing 6."], "premise": "Thing 5", "conclusion": "Therefore not Thing 2."}, {"world": ["If Thing 1 then Thing 2.", "If Thing 2 then Thing 3.", "If Thing 3 then Thing 4.", "If Thing 4 then Thing 5.", "If Thing 5 then Thing 6."], "premise": "not Thing 5", "conclusion": "Therefore Thing 2."}, {"world": ["If Thing 1 then Thing 2.", "If Thing 2 then Thing 3.", "If Thing 3 then Thing 4.", "If Thing 4 then Thing 5.", "If Thing 5 then Thing 6."], "premise": "not Thing 5", "conclusion": "Therefore not Thing 2."}, {"world": ["If Thing 1 then Thing 2.", "If Thing 2 then Thing 3.", "If Thing 3 then Thing 4.", "If Thing 4 then Thing 5.", "If Thing 5 then Thing 6."], "premise": "Thing 5", "conclusion": "Therefore Thing 3."}, {"world": ["If Thing 1 then Thing 2.", "If Thing 2 then Thing 3.", "If Thing 3 then Thing 4.", "If Thing 4 then Thing 5.", "If Thing 5 then Thing 6."], "premise": "Thing 5", "conclusion": "Therefore not Thing 3."}, {"world": ["If Thing 1 then Thing 2.", "If Thing 2 then Thing 3.", "If Thing 3 then Thing 4.", "If Thing 4 then Thing 5.", "If Thing 5 then Thing 6."], "premise": "not Thing 5", "conclusion": "Therefore Thing 3."}, {"world": ["If Thing 1 then Thing 2.", "If Thing 2 then Thing 3.", "If Thing 3 then Thing 4.", "If Thing 4 then Thing 5.", "If Thing 5 then Thing 6."], "premise": "not Thing 5", "conclusion": "Therefore not Thing 3."}, {"world": ["If Thing 1 then Thing 2.", "If Thing 2 then Thing 3.", "If Thing 3 then Thing 4.", "If Thing 4 then Thing 5.", "If Thing 5 then Thing 6."], "premise": "Thing 5", "conclusion": "Therefore Thing 4."}, {"world": ["If Thing 1 then Thing 2.", "If Thing 2 then Thing 3.", "If Thing 3 then Thing 4.", "If Thing 4 then Thing 5.", "If Thing 5 then Thing 6."], "premise": "Thing 5", "conclusion": "Therefore not Thing 4."}, {"world": ["If Thing 1 then Thing 2.", "If Thing 2 then Thing 3.", "If Thing 3 then Thing 4.", "If Thing 4 then Thing 5.", "If Thing 5 then Thing 6."], "premise": "not Thing 5", "conclusion": "Therefore Thing 4."}, {"world": ["If Thing 1 then Thing 2.", "If Thing 2 then Thing 3.", "If Thing 3 then Thing 4.", "If Thing 4 then Thing 5.", "If Thing 5 then Thing 6."], "premise": "not Thing 5", "conclusion": "Therefore not Thing 4."}, {"world": ["If Thing 2 then Thing 5.", "If Thing 3 then Thing 1.", "If Thing 3 then Thing 6.", "If Thing 4 then Thing 5.", "If Thing 6 then Thing 1."], "premise": "Thing 1", "conclusion": "Therefore Thing 3."}, {"world": ["If Thing 2 then Thing 5.", "If Thing 3 then Thing 1.", "If Thing 3 then Thing 6.", "If Thing 4 then Thing 5.", "If Thing 6 then Thing 1."], "premise": "Thing 1", "conclusion": "Therefore not Thing 3."}, {"world": ["If Thing 2 then Thing 5.", "If Thing 3 then Thing 1.", "If Thing 3 then Thing 6.", "If Thing 4 then Thing 5.", "If Thing 6 then Thing 1."], "premise": "not Thing 1", "conclusion": "Therefore Thing 3."}, {"world": ["If Thing 2 then Thing 5.", "If Thing 3 then Thing 1.", "If Thing 3 then Thing 6.", "If Thing 4 then Thing 5.", "If Thing 6 then Thing 1."], "premise": "not Thing 1", "conclusion": "Therefore not Thing 3."}, {"world": ["If Thing 2 then Thing 5.", "If Thing 3 then Thing 1.", "If Thing 3 then Thing 6.", "If Thing 4 then Thing 5.", "If Thing 6 then Thing 1."], "premise": "Thing 5", "conclusion": "Therefore Thing 2."}, {"world": ["If Thing 2 then Thing 5.", "If Thing 3 then Thing 1.", "If Thing 3 then Thing 6.", "If Thing 4 then Thing 5.", "If Thing 6 then Thing 1."], "premise": "Thing 5", "conclusion": "Therefore not Thing 2."}, {"world": ["If Thing 2 then Thing 5.", "If Thing 3 then Thing 1.", "If Thing 3 then Thing 6.", "If Thing 4 then Thing 5.", "If Thing 6 then Thing 1."], "premise": "not Thing 5", "conclusion": "Therefore Thing 2."}, {"world": ["If Thing 2 then Thing 5.", "If Thing 3 then Thing 1.", "If Thing 3 then Thing 6.", "If Thing 4 then Thing 5.", "If Thing 6 then Thing 1."], "premise": "not Thing 5", "conclusion": "Therefore not Thing 2."}, {"world": ["If Thing 2 then Thing 5.", "If Thing 3 then Thing 1.", "If Thing 3 then Thing 6.", "If Thing 4 then Thing 5.", "If Thing 6 then Thing 1."], "premise": "Thing 5", "conclusion": "Therefore Thing 4."}, {"world": ["If Thing 2 then Thing 5.", "If Thing 3 then Thing 1.", "If Thing 3 then Thing 6.", "If Thing 4 then Thing 5.", "If Thing 6 then Thing 1."], "premise": "Thing 5", "conclusion": "Therefore not Thing 4."}, {"world": ["If Thing 2 then Thing 5.", "If Thing 3 then Thing 1.", "If Thing 3 then Thing 6.", "If Thing 4 then Thing 5.", "If Thing 6 then Thing 1."], "premise": "not Thing 5", "conclusion": "Therefore Thing 4."}, {"world": ["If Thing 2 then Thing 5.", "If Thing 3 then Thing 1.", "If Thing 3 then Thing 6.", "If Thing 4 then Thing 5.", "If Thing 6 then Thing 1."], "premise": "not Thing 5", "conclusion": "Therefore not Thing 4."}]
+const all_stimuli = [
+  {
+    world: [
+      "If flemp then glork",
+      "If glork then dalx",
+      "If dalx then zilft",
+      "If zilft then quarg",
+      "If quarg then blick",
+    ],
+    premise: "flemp",
+    conclusion: "Therefore blick",
+  },
+  {
+    world: [
+      "If flemp then glork",
+      "If glork then dalx",
+      "If dalx then zilft",
+      "If zilft then quarg",
+      "If quarg then blick",
+    ],
+    premise: "not quarg",
+    conclusion: "Therefore not flemp",
+  },
+  {
+    world: [
+      "If flemp then glork",
+      "If glork then dalx",
+      "If dalx then zilft",
+      "If zilft then quarg",
+      "If quarg then blick",
+    ],
+    premise: "not glork",
+    conclusion: "Therefore not zilft",
+  },
+  {
+    world: [
+      "If flemp then glork",
+      "If glork then dalx",
+      "If dalx then zilft",
+      "If zilft then quarg",
+      "If quarg then blick",
+    ],
+    premise: "blick",
+    conclusion: "Therefore dalx",
+  },
+  {
+    world: [
+      "If it is winter then it will be cold",
+      "If it is cold then it will snow",
+      "If it snows then the roads will be icy",
+      "If the roads are icy then there will be car accidents",
+      "If there are car accidents then there will be traffic jams",
+    ],
+    premise: "It is winter",
+    conclusion: "Therefore the roads will be icy",
+  },
+  {
+    world: [
+      "If it is winter then it will be cold",
+      "If it is cold then it will snow",
+      "If it snows then the roads will be icy",
+      "If the roads are icy then there will be car accidents",
+      "If there are car accidents then there will be traffic jams",
+    ],
+    premise: "It does not snow",
+    conclusion: "Therefore there will not be car accidents",
+  },
+  {
+    world: [
+      "If it is winter then it will be cold",
+      "If it is cold then it will snow",
+      "If it snows then the roads will be icy",
+      "If the roads are icy then there will be car accidents",
+      "If there are car accidents then there will be traffic jams",
+    ],
+    premise: "There are not traffic jams",
+    conclusion: "Therefore it is not cold",
+  },
+  {
+    world: [
+      "If it is winter then it will be cold",
+      "If it is cold then it will snow",
+      "If it snows then the roads will be icy",
+      "If the roads are icy then there will be car accidents",
+      "If there are car accidents then there will be traffic jams",
+    ],
+    premise: "It snows",
+    conclusion: "Therefore it is winter",
+  },
+  {
+    world: [
+      "If it is night then the sun will be in the sky.",
+      "If the sun is in the sky then it will be bright outside.",
+      "If it is bright outside then people will wear sunglasses.",
+      "If people wear sunglasses then people will grow wings.",
+      "If people grow wings then they will burrow underground.",
+    ],
+    premise: "It is night.",
+    conclusion: "Therefore people will wear sunglasses.",
+  },
+  {
+    world: [
+      "If it is night then the sun will be in the sky.",
+      "If the sun is in the sky then it will be bright outside.",
+      "If it is bright outside then people will wear sunglasses.",
+      "If people wear sunglasses then people will grow wings.",
+      "If people grow wings then they will burrow underground.",
+    ],
+    premise: "People do not wear sunglasses.",
+    conclusion: "Therefore people do not burrow underground.",
+  },
+  {
+    world: [
+      "If it is night then the sun will be in the sky.",
+      "If the sun is in the sky then it will be bright outside.",
+      "If it is bright outside then people will wear sunglasses.",
+      "If people wear sunglasses then people will grow wings.",
+      "If people grow wings then they will burrow underground.",
+    ],
+    premise: "People do not burrow underground.",
+    conclusion: "Therefore it is not bright outside.",
+  },
+  {
+    world: [
+      "If it is night then the sun will be in the sky.",
+      "If the sun is in the sky then it will be bright outside.",
+      "If it is bright outside then people will wear sunglasses.",
+      "If people wear sunglasses then people will grow wings.",
+      "If people grow wings then they will burrow underground.",
+    ],
+    premise: "Therefore the sun is in the sky.",
+    conclusion: "Therefore people grow wings.",
+  },
+];
+
+const practiceStimulus = {
+  world: [
+    "If the sprinkler turns on then the grass will be wet.",
+    "If the grass is wet then the ground will be muddy.",
+  ],
+  premise: "The sprinkler turns on.",
+  conclusion: "Therefore the ground is muddy.",
+};
--- experiment.js
+++ experiment.js
@@ -1,20 +1,27 @@
 // JavaScript for syllogistic reasoning experiment
 
 // constants
-const trialDuration = 10000; // 10 seconds
+const nSeconds = 20;
+const trialDuration = nSeconds * 1000; // 20 seconds
 
-const renderStimulus = function (stimulus) {
-  const statements = `<div class="world-statements">${shuffle(stimulus.world)
+// functions for rendering stimulus
+const renderWorld = function (world) {
+  const statements = `<div class="world-statements"><ul>${world
     .map((s) => `<li>${s}</li>`)
-    .join("")}</div>`;
+    .join("")}</ul></div>`;
+
+  return statements;
+};
+const renderStimulus = function (stimulus, world) {
+  const worldStatements = renderWorld(world);
   const premise = `<p><strong>Premise:</strong> ${stimulus.premise}</p>`;
   const question = `<p><strong>Conclusion:</strong> ${stimulus.conclusion}</p>`;
-  const html = `<div class='stimulus'><ul>${statements}</ul>${premise}${question}</div>`;
+  const html = `<div class='stimulus'>${worldStatements}${premise}${question}</div>`;
   return html;
 };
 
+// Fisher-Yates shuffle, from here: https://stackoverflow.com/a/2450976
 function shuffle(array) {
-  // Fisher-Yates shuffle, from here: https://stackoverflow.com/a/2450976
   let currentIndex = array.length,
     randomIndex;
 
@@ -44,30 +51,70 @@ const jsPsych = initJsPsych({
 
 const rawCondition = jsPsych.data.getURLVariable("condition");
 const isSpeeded = rawCondition == 1;
+const speededInstruction = isSpeeded
+  ? `After seeing the premise and conclusion, you will have only ${numSeconds} seconds to make a decision.`
+  : "";
 
-const instructions = {
+// consent form and instructions
+const consent = {
   type: jsPsychInstructions,
   pages: [
     "<p class='instructions-text'>By answering the following questions, you are participating in a study being performed by cognitive scientists in the Stanford Department of Psychology.\
          If you have questions about this research, please contact <strong>Ben Prystawski</strong> at <a href='mailto:benpry@stanford.edu'>benpry@stanford.edu</a> or\
          Noah Goodman at ngoodman@stanford.edu. You must be at least 18 years old to participate. Your participation in this research is voluntary.\
-         You may decline to answer any or all of the following questions. You may decline further participation, at any time, without adverse consequences.\
+         You may decline to answer any or pll of the following questions. You may decline further participation, at any time, without adverse consequences.\
          Your anonymity is assured; the researchers who have requested your participation will not receive any personal information about you.</p>",
-    "<p class='instructions-text'>This is an experiment investigating how people reason. You will be given twelve problems. In each case, you will be given passage\
-         to read and asked if a certain conclusion may be logically deduced from it. You should answer this question on the assumption that all the information \
-         given in the passage is, in fact, true. If you judge that the conclusion necessarily follows from the statements in the passage, you should answer 'yes,' otherwise 'no.'</p>",
+  ],
+  show_clickable_nav: true,
+};
+const instructions = {
+  type: jsPsychInstructions,
+  pages: [
+    `<p class='instructions-text'>This is an experiment investigating how people reason. You will be given twelve problems. In each case, you will be given a series
+         of statements to read, then you will be presented with a series of five if-then statements. For the task, you should pretend all of these sentences are true,
+         whether or not they are true in the real world. Next, you will be given a premise and a conclusion and asked to determine whether the conclusion follows
+         logically from the premise.${speededInstruction}</p>`,
+    `<p class='instructions-text'>Next, you will complete a practice trial to familiarize you with the task. Afterward, you will move on to the main trials.</p>`,
   ],
   show_clickable_nav: true,
 };
 
-const trials = [instructions];
+// set up practice trials
+const prePractice = {
+  type: jsPsychHtmlButtonResponse,
+  stimulus: `<div class="stimulus">${renderWorld(
+    practiceStimulus.world,
+  )}</div>`,
+  choices: ["start"],
+  prompt: `<p>Study this list of statements, then click  "start" to see a question.</p>`,
+  button_html: `<button class='jspsych-btn' style="font-size:18pt">%choice%</button>`,
+};
+const practice = {
+  type: jsPsychHtmlButtonResponse,
+  prompt:
+    '<p>Choose "yes" if the conclusion follows from the premise, and "no" otherwise.</p>',
+  stimulus: renderStimulus(practiceStimulus, practiceStimulus.world),
+  choices: ["yes", "no"],
+  on_load: isSpeeded ? startTimer : null,
+  on_finish: isSpeeded ? stopTimer : null,
+  trial_duration: isSpeeded ? trialDuration : null,
+  button_html: [
+    `<button class='jspsych-btn' style="font-size:18pt">%choice%</button>`,
+    `<button class='jspsych-btn' style="font-size:18pt">%choice%</button>`,
+  ],
+};
 
-shuffle(stimuli);
+const trials = [consent, instructions, prePractice, practice];
+
+const stimuli = shuffle(all_stimuli);
 stimuli.map((s, i) => {
+  const sWorld = shuffle(s.world);
+
   const preTrial = {
     type: jsPsychHtmlButtonResponse,
-    stimulus: "Press 'start' to begin the trial",
+    stimulus: `<div class="stimulus">${renderWorld(sWorld)}</div>`,
     choices: ["start"],
+    prompt: `<p>Study this list of statements, then click  "start" to see a question.</p>`,
     button_html: `<button class='jspsych-btn' style="font-size:18pt">%choice%</button>`,
   };
   trials.push(preTrial);
@@ -75,15 +122,14 @@ stimuli.map((s, i) => {
     type: jsPsychHtmlButtonResponse,
     prompt:
       '<p>Choose "yes" if the conclusion follows from the premise, and "no" otherwise.</p>',
-
-    stimulus: renderStimulus(s),
+    stimulus: renderStimulus(s, sWorld),
     choices: ["yes", "no"],
     on_load: isSpeeded ? startTimer : null,
     on_finish: isSpeeded ? stopTimer : null,
     trial_duration: isSpeeded ? trialDuration : null,
     button_html: [
-      `<button class='jspsych-btn' style="background:#3ab059;font-size:18pt">%choice%</button>`,
-      `<button class='jspsych-btn' style="background:#d94e3f;font-size:18pt">%choice%</button>`,
+      `<button class='jspsych-btn' style="font-size:18pt">%choice%</button>`,
+      `<button class='jspsych-btn' style="font-size:18pt">%choice%</button>`,
     ],
   };
   trials.push(trial);
